05ae1bd27c7120ad18f450e6c69bf0cc2aace589
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2023-10-31 10:13:49 -0400

    layout on case study page

--- src/templates/case-study-template.js
+++ src/templates/case-study-template.js
@@ -2,76 +2,79 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import Content from '../components/content'
+import Layout from '../components/layout'
 
 const CaseStudy = ({ data }) => {
   const { client, year, location, headerImage, content, headerText } =
     data.contentfulCaseStudy
   return (
-    <div className='case-study-page'>
-      {headerImage && !headerText && (
-        <div className='header-image'>
-          <GatsbyImage
-            image={headerImage.image.gatsbyImageData}
-            alt={headerImage.image.description}
-          ></GatsbyImage>
-          <p className='header-image-caption'>{headerImage.caption}</p>
-        </div>
-      )}
-      {headerImage && headerText && (
-        <div className='header-image-with-text'>
-          <div className='header-image-with-text-image'>
+    <Layout>
+      <div className='case-study-page'>
+        {headerImage && !headerText && (
+          <div className='header-image'>
             <GatsbyImage
               image={headerImage.image.gatsbyImageData}
               alt={headerImage.image.description}
             ></GatsbyImage>
             <p className='header-image-caption'>{headerImage.caption}</p>
           </div>
+        )}
+        {headerImage && headerText && (
+          <div className='header-image-with-text'>
+            <div className='header-image-with-text-image'>
+              <GatsbyImage
+                image={headerImage.image.gatsbyImageData}
+                alt={headerImage.image.description}
+              ></GatsbyImage>
+              <p className='header-image-caption'>{headerImage.caption}</p>
+            </div>
+            <div
+              className='header-image-with-text-text'
+              dangerouslySetInnerHTML={{
+                __html: headerText.childMarkdownRemark.html,
+              }}
+            ></div>
+          </div>
+        )}
+        {headerText && !headerImage && (
           <div
-            className='header-image-with-text-text'
+            className='text-header'
             dangerouslySetInnerHTML={{
               __html: headerText.childMarkdownRemark.html,
             }}
           ></div>
+        )}
+        <div className='case-study-body'>
+          <div className='case-study-left'>
+            {client && (
+              <div>
+                <p>
+                  <em>Client</em>
+                </p>
+                <p>{client}</p>
+              </div>
+            )}
+            {year && (
+              <div>
+                <p>
+                  <em>Year</em>
+                </p>
+                <p>{year}</p>
+              </div>
+            )}
+            {location && (
+              <div>
+                <p>
+                  <em>Location</em>
+                </p>
+                <p>{location}</p>
+              </div>
+            )}
+          </div>
+          {content && <Content content={content}></Content>}
         </div>
-      )}
-      {headerText && !headerImage && (
-        <div
-          className='text-header'
-          dangerouslySetInnerHTML={{
-            __html: headerText.childMarkdownRemark.html,
-          }}
-        ></div>
-      )}
-      <div className='case-study-body'>
-        <div className='case-study-left'>
-          {client && (
-            <div>
-              <p>
-                <em>Client</em>
-              </p>
-              <p>{client}</p>
-            </div>
-          )}
-          {year && (
-            <div>
-              <p>
-                <em>Year</em>
-              </p>
-              <p>{year}</p>
-            </div>
-          )}
-          {location && (
-            <div>
-              <p>
-                <em>Location</em>
-              </p>
-              <p>{location}</p>
-            </div>
-          )}
-        </div>
-        {content && <Content content={content}></Content>}
       </div>
-    </div>
+    </Layout>
   )
 }
 
