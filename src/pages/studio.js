a834ef215bee18de592ac9d63353d714781fcbb6
PhilLanzetta <71086048+PhilLanzetta@users.noreply.github.com>
2024-03-07 10:03:29 -0500

    list view

--- src/pages/studio.js
+++ src/pages/studio.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/layout'
 import { graphql, Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 import { Fade } from 'react-awesome-reveal'
+import { RiLayoutGridFill } from 'react-icons/ri'
+import { PiListBold } from 'react-icons/pi'
+import { RiArrowRightLine } from 'react-icons/ri'
 
 const Studio = ({ data, location }) => {
   const caseStudy = data.allContentfulCaseStudy.nodes
+  const [view, setView] = useState()
+
+  useEffect(() => {
+    if (localStorage.getItem('view')) {
+      setView(localStorage.getItem('view'))
+    } else {
+      setView('grid')
+    }
+  }, [])
 
   return (
     <Layout location={location}>
@@ -15,26 +27,94 @@ const Studio = ({ data, location }) => {
         <Link to='/studio' activeClassName='active-filter-button'>
           Case Studies
         </Link>
+        <button
+          className={`view-button ${view === 'grid' ? '' : 'faded'}`}
+          onClick={() => {
+            localStorage.setItem('view', 'grid')
+            setView('grid')
+          }}
+        >
+          Grid
+          <RiLayoutGridFill></RiLayoutGridFill>
+        </button>
+        <button
+          className={`view-button ${view === 'list' ? '' : 'faded'}`}
+          onClick={() => {
+            localStorage.setItem('view', 'list')
+            setView('list')
+          }}
+        >
+          List
+          <PiListBold></PiListBold>
+        </button>
       </div>
-      <div className='projects-container'>
-        <div className='project-tiles-container'>
-          {caseStudy.map((project) => (
-            <Fade className='project-tile' triggerOnce={true} key={project.id}>
-              <Link to={`/studio/${project.slug}`}>
-                {project.tileImage && (
-                  <GatsbyImage
-                    image={project.tileImage.image.gatsbyImageData}
-                    alt={project.tileImage.image.description}
-                  ></GatsbyImage>
-                )}
-                <p className='project-tile-title'>
-                  <em>{project.title}</em>
-                </p>
-                <p>{project.subtitle}</p>
-              </Link>
-            </Fade>
-          ))}
-        </div>
+      <div
+        className={
+          view === 'grid' ? 'projects-container' : 'list-projects-container'
+        }
+      >
+        {view === 'grid' && (
+          <div className='project-tiles-container'>
+            {caseStudy.map((project) => (
+              <Fade
+                className='project-tile'
+                triggerOnce={true}
+                key={project.id}
+              >
+                <Link to={`/studio/${project.slug}`}>
+                  {project.tileImage && (
+                    <GatsbyImage
+                      image={project.tileImage.image.gatsbyImageData}
+                      alt={project.tileImage.image.description}
+                    ></GatsbyImage>
+                  )}
+                  <p className='project-tile-title'>
+                    <em>{project.title}</em>
+                  </p>
+                  <p>{project.subtitle}</p>
+                </Link>
+              </Fade>
+            ))}
+          </div>
+        )}
+        {view === 'list' && (
+          <div className='project-list-container'>
+            {caseStudy.map((project) => (
+              <Fade triggerOnce={true} key={project.id}>
+                <Link
+                  to={`/studio/${project.slug}`}
+                  className='project-listing'
+                >
+                  <div className='project-listing-text'>
+                    <p>{project.title}</p>
+                    <p>
+                      <em>{project.subtitle}</em>
+                    </p>
+                    <p>
+                      <ul className='listing-scope'>
+                        {project.scope?.map((item, index) => (
+                          <li key={index}>{item}</li>
+                        ))}
+                      </ul>
+                    </p>
+                    <p>
+                      <span className='list-view-go'>
+                        View Case Study <RiArrowRightLine></RiArrowRightLine>
+                      </span>
+                    </p>
+                  </div>
+                  {project.tileImage && (
+                    <GatsbyImage
+                      image={project.tileImage.image.gatsbyImageData}
+                      alt={project.tileImage.image.description}
+                      className='listing-image'
+                    ></GatsbyImage>
+                  )}
+                </Link>
+              </Fade>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   )
@@ -56,6 +136,7 @@ export const query = graphql`
         }
         slug
         isFeatured
+        scope
         subtitle
         title
       }
